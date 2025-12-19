28a110ab66cc1ef66711b36078cf0109885dd324
elpwc/wniko <elpwc@hotmail.com>
2025-12-20 01:55:11 +0900

    feat: Show today's station on top of RankingModal

--- src/components/modals/RankingModal.tsx
+++ src/components/modals/RankingModal.tsx
@@ -16,7 +16,7 @@ export const RankingModal = ({ show, onClose, onChallengeButtonOnClick }: Props)
 
 	useEffect(() => {
 		getQuestionsList({
-			before: new Date(),
+			before: new Date(new Date().setDate(new Date().getDate() + 1)),
 			limit: 100,
 			orderBy: OrderType.date,
 			asc: false,
@@ -41,33 +41,49 @@ export const RankingModal = ({ show, onClose, onChallengeButtonOnClick }: Props)
 					const stationInfo = getStationById(question.stationId);
 					return (
 						<div key={index} className="justify-between border-b border-gray-300 pb-2">
-							<div>
+							<div className="flex gap-2 items-center">
 								<p>
 									{new Date(question.showAt).toLocaleDateString('ja-JP', {
 										timeZone: 'UTC',
 									})}
 								</p>
+
+								{index === 0 ? <p className="px-2 bg-blue-500 text-white text-[10px] rounded-[4px]">今日</p> : <></>}
 							</div>
 							<div className="grid grid-cols-10 justify-between items-center">
 								<p className="col-span-6">{`${stationInfo.pref} ${question.maskedStationName}駅`}</p>
-								<p className="col-span-1">
-									{question.complete && question.challenge ? Math.round((question.complete / question.challenge) * 100) : 0}%
-									{/* ({question.complete || 0}/{question.challenge || 0}) */}
-								</p>
-								<RingProgress
-									className="col-span-1 w-4"
-									progress={question.complete && question.challenge ? Math.round((question.complete / question.challenge) * 100) : 0}
-									forecolor="green"
-								/>
-								<button
-									className="col-span-2"
-									style={{ margin: 0 }}
-									onClick={() => {
-										onChallengeButtonOnClick({ stationId: question.stationId, maskedStationName: question.maskedStationName });
-									}}
-								>
-									挑戦
-								</button>
+
+								{index === 0 ? (
+									<p className="col-span-1 text-[12px]">統計中</p>
+								) : (
+									<p className="col-span-1">
+										{question.complete && question.challenge ? Math.round((question.complete / question.challenge) * 100) : 0}%
+										{/* ({question.complete || 0}/{question.challenge || 0}) */}
+									</p>
+								)}
+
+								{index === 0 ? (
+									<div></div>
+								) : (
+									<RingProgress
+										className="col-span-1 w-4"
+										progress={question.complete && question.challenge ? Math.round((question.complete / question.challenge) * 100) : 0}
+										forecolor="green"
+									/>
+								)}
+								{index === 0 ? (
+									<div className="font-bold col-span-2 text-center text-sky-600 text-[12px] px-2 py-1">今日の駅</div>
+								) : (
+									<button
+										className="col-span-2 text-[14px]"
+										style={{ margin: 0 }}
+										onClick={() => {
+											onChallengeButtonOnClick({ stationId: question.stationId, maskedStationName: question.maskedStationName });
+										}}
+									>
+										挑戦
+									</button>
+								)}
 							</div>
 						</div>
 					);
